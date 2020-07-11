abd96cea0c9f628b0e1c0173f5e2084088fad06f
Or Lavy <orlavyil@gmail.com>
2020-07-11 16:38:43 +0300

    Updates

---
+++ precache-manifest.95a6cfc58e8f738670ade9564435b493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4838601d2f52eb2de796f865ecdc614d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b963b5b4f01902d9da2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60eb87aa7562f3d951c9",
+    "url": "/static/js/2.3db4bc9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3db4bc9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b963b5b4f01902d9da2",
+    "url": "/static/js/main.5556f842.chunk.js"
+  },
+  {
+    "revision": "22065e8be63650a2fa67",
+    "url": "/static/js/runtime-main.3c9723f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
