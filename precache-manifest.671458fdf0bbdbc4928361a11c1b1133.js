d46215983d0a74f51650b1e195ed42533f927095
Or Lavy <orlavyil@gmail.com>
2020-07-11 16:44:20 +0300

    Updates

---
+++ precache-manifest.671458fdf0bbdbc4928361a11c1b1133.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92632fc92072fa7effb80344c9b11e2",
+    "url": "//bambelonja.github.io/the-question/index.html"
+  },
+  {
+    "revision": "94e58802a259b5e3f983",
+    "url": "//bambelonja.github.io/the-question/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60eb87aa7562f3d951c9",
+    "url": "//bambelonja.github.io/the-question/static/js/2.3db4bc9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//bambelonja.github.io/the-question/static/js/2.3db4bc9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e58802a259b5e3f983",
+    "url": "//bambelonja.github.io/the-question/static/js/main.978c480c.chunk.js"
+  },
+  {
+    "revision": "0ffb3bc518bcfdeb1883",
+    "url": "//bambelonja.github.io/the-question/static/js/runtime-main.044b01f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//bambelonja.github.io/the-question/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db6cca2a5a5e97a489c3116bdd323ce1.js"
+  "//bambelonja.github.io/the-question/precache-manifest.671458fdf0bbdbc4928361a11c1b1133.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//bambelonja.github.io/the-question/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
